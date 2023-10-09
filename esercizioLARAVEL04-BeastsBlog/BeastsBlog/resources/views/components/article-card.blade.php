730e3d98db34d5686e0ff811cc3c21ca669294ce
Francesco.Ingegnere <geom.ingegnere@gmail.com>
2023-10-09 18:40:11 +0200

    stilizzata card articoli

--- esercizioLARAVEL04-BeastsBlog/BeastsBlog/resources/views/components/article-card.blade.php
+++ esercizioLARAVEL04-BeastsBlog/BeastsBlog/resources/views/components/article-card.blade.php
@@ -5,7 +5,7 @@
     <div class="card-body">
         <h5 class="card-title">{{$title}}</h5>
         <p class="card-text">{{$description}}</p>
-        <a href="{{$route}}" class="btn btn-primary">Continua ...</a>
+        <button class="btn btn-primary"><a href="{{$route}}" class="text-decoration-none p-auto">Continua ...</a></button>
     </div>
     <div class="card-footer text-body-secondary">
         2 days ago
