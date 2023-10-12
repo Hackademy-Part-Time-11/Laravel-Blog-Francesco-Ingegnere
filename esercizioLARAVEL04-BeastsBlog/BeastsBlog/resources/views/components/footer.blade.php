6b696719e97a9242fec74e124564fa13db28d78e
Francesco.Ingegnere <geom.ingegnere@gmail.com>
2023-10-12 20:38:57 +0200

    modificato button footer

--- esercizioLARAVEL04-BeastsBlog/BeastsBlog/resources/views/components/footer.blade.php
+++ esercizioLARAVEL04-BeastsBlog/BeastsBlog/resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
                 <h6 class="py-2">Scrivi e pubblica il tuo articolo BESTIALE su BEASTS-BLOG</h6>
             </div>
             <div class="col text-end">
-                <button type="button" class="btn btn-primary px-5" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"><i class="fa-solid fa-square-phone"></i> Contattaci</button>
+                <a href="{{route('contacts')}}"><button type="button" class="btn btn-primary px-5" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"><i class="fa-solid fa-square-phone"></i> Contattaci</button></a>
             </div>
         </div>
         
