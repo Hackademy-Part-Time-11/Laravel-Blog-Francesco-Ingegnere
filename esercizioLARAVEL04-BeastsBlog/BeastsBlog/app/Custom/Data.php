81e082fae7aa619fa76a96fd46bd7d63fc8cf2c6
Francesco.Ingegnere <geom.ingegnere@gmail.com>
2023-10-14 12:44:37 +0200

    add in data code database table

--- esercizioLARAVEL04-BeastsBlog/BeastsBlog/app/Custom/Data.php
+++ esercizioLARAVEL04-BeastsBlog/BeastsBlog/app/Custom/Data.php
@@ -80,3 +80,120 @@ Ref: "users"."id" < "orders"."user_id"
 Ref: "books"."id" < "orders"."book_id"
 
 */
+
+/* 
+
+E-COMMERCE
+
+// Use DBML to define your database structure
+// Docs: https://dbml.dbdiagram.io/docs
+
+Table users {
+  id biginteger [primary key]
+  name varchar
+  surname varchar
+  birth_date timestamp
+  email email
+  address varchar
+  phone integer
+}
+
+Table books {
+  id biginteger [primary key]
+  title varchar
+  author varchar
+  year timestamp [note: 'year of publication']
+  quantity integer [note: 'quantity in stock']
+}
+
+Table orders {
+  id integer [primary key]
+  user_id biginteger
+  book_id biginteger
+  order_date timestamp
+  payment_state integer
+  shipping_address varchar
+}
+
+
+
+Ref: "users"."id" < "orders"."user_id"
+
+Ref: "books"."id" < "orders"."book_id"
+
+*/
+
+/* 
+
+SOCIAL NETWORK
+
+// Use DBML to define your database structure
+// Docs: https://dbml.dbdiagram.io/docs
+
+Table users {
+  id biginteger [primary key]
+  username varchar
+  email email
+  photo photo
+}
+
+Table posts {
+  id biginteger [primary key]
+  body text [note: 'Content of the post']
+  created_at timestamp
+  user_id biginteger
+}
+
+Table likes {
+  id biginteger [primary key]
+  user_id biginteger
+  post_id biginteger
+  created_at timestamp
+}
+
+
+Ref: "users"."id" < "posts"."user_id"
+
+Ref: "users"."id" < "likes"."user_id"
+
+Ref: "posts"."id" < "likes"."post_id"
+
+*/
+
+/* 
+
+SITO DI ANNUNCI
+
+// Use DBML to define your database structure
+// Docs: https://dbml.dbdiagram.io/docs
+
+Table users {
+  id biginteger [primary key]
+  name varchar
+  surname varchar
+  birth_date timestamp
+  email email
+  role varchar
+}
+
+Table announcements {
+  id biginteger [primary key]
+  title varchar
+  body text [note: 'Content of the announcement']
+  user_id biginteger
+  category_id biginteger
+  created_at timestamp
+  status integer
+}
+
+Table categories {
+  id biginteger [primary key]
+  name varchar
+}
+
+
+Ref: "categories"."id" <> "announcements"."category_id"
+
+Ref: "users"."id" < "announcements"."user_id"
+
+*/
