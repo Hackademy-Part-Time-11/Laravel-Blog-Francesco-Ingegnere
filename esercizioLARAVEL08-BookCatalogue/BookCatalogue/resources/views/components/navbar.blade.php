e9f9aae76d69c6be2c056c06531e4afe0fe96c72
Francesco.Ingegnere <geom.ingegnere@gmail.com>
2023-10-26 21:11:31 +0200

    sistemati tasti navbar

--- esercizioLARAVEL08-BookCatalogue/BookCatalogue/resources/views/components/navbar.blade.php
+++ esercizioLARAVEL08-BookCatalogue/BookCatalogue/resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('home')}}">Library</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Library</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,25 +8,32 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown link
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
+
+                @auth
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('books.create') }}">Add Book</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('books.index') }}">Your Books</a>
+                    </li>
+
+                @endauth
+
+                @guest
+
+                    <a href="{{ route('login') }}" class="btn btn-dark">LOGIN</a>
+                    <a href="{{ route('register') }}" class="btn btn-dark">REGISTER</a>
+                @else
+
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">LOGOUT</button>
+                    </form>
+
+                @endguest
             </ul>
         </div>
     </div>
