d2bc95c07583345aceab94098f54ee8be0b507b5
Francesco.Ingegnere <geom.ingegnere@gmail.com>
2023-11-07 19:30:17 +0100

    aggiustato factory immagine articoli

--- es-04-BeastsBlog/BeastsBlog/database/factories/ArticleFactory.php
+++ es-04-BeastsBlog/BeastsBlog/database/factories/ArticleFactory.php
@@ -17,13 +17,15 @@ class ArticleFactory extends Factory
      */
     public function definition(): array
     {
+        $indirizzo = 'C:\Users\Francesco\Dropbox\02_FRANCESCO\01_PROGRAMMAZIONE\02_AULAB\07_Laravel\04_esercizi_LARAVEL\es-04-BeastsBlog\BeastsBlog\storage\app/';
+
         return [
             'user_id' => rand(1, 20),
             'title' => fake()->words(rand(3, 6), true),
             'description' => fake()->words(rand(6, 15), true),
             'visible' => 1,
             'body' => fake()->text(rand(150, 300)),
-            'image' => fake()->image(null ,640, 480, 'animals', true),
+            'image' => str_replace($indirizzo, '', fake()->image(storage_path('app/public/articles') ,640, 480, 'animals', true)),
         ];
     }
 }
